5e4ab4c8b6c8e7a540246affdbf5b999fee25ec9
Maira Marques Samary <marquemo@bc.edu>
2023-05-11 15:04:15 -0400

    correcting CSRF

--- MyTestProject/MyTestProject/settings.py
+++ MyTestProject/MyTestProject/settings.py
@@ -130,5 +130,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 CSRF_TRUSTED_ORIGINS = [
-https://web-production-fcfa.up.railway.app,
+'https://deployexample-production.up.railway.app',
 ]
